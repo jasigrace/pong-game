bb5ab0172049524012f5e2205ff2e0a15976aa1f
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-05-16 06:17:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import time
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("The Pong Game")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+# Draw a center line
+for position in range(-20, 20):
+    line = Turtle()
+    line.shape("square")
+    line.color("white")
+    line.penup()
+    line.shapesize(0.5, 0.1)
+    line.goto(0, position * -20)
+
+r_paddle = Paddle(350)
+l_paddle = Paddle(-350)
+
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < - 320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    elif ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    elif ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
